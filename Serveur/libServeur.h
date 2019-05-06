916cce43cb8f71f8c04b5d9b0542b36ebc292d4c
Romain  BOISSON <romain.nicolas.boisson@gmail.com>
2019-05-06 15:35:49 +0200

    suppresion de printf dns le client.c et ajouts des connexion dans le serveur

--- Serveur/libServeur.h
+++ Serveur/libServeur.h
@@ -14,4 +14,20 @@
 
 #define BOARD_SIZE 5
 
+/* Fonction permettant de vérifié les érreurs RÉSEAU sur la fonction RECV
+  params:
+    int err : valeur de retour de la fonction recv
+    int sock : socket connecté au serveur
+ */
+void checkRecvrError(int err,int sock);
+
+/* Fonction permettant de vérifié les érreurs RÉSEAU sur la fonction SEND
+  params:
+    int err : valeur de retour de la fonction recv
+    int sock : socket connecté au serveur
+ */
+void checkSendError(int err,int sock );
+
+int sendAnswers(int sockP1,int sockP2,TPartieReq player1,TPartieReq player2);
+
 #endif
